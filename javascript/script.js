8c405287758229d020bdf7f9d0bb367ab775ca95
Bryan Rainbow <bryanrbow@gmail.com>
2020-12-05 15:32:19 -0800

    Add function for dark/light theme

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,36 @@ function hide_input_area() {
         hidden_elements[i].style.display = "none";
     }
 }
+
+function dark_light_theme() {
+    var newnote_button = document.getElementById("newnote-button");
+    var dark_button = document.getElementById("darktheme-button");
+    var save_button = document.getElementById("save-button");
+    var cancel_button = document.getElementById("cancel-button");
+    var aside_area = document.getElementsByTagName("aside");
+    var content_area = document.getElementsByTagName("content");
+    var text_box = document.getElementsByTagName("textarea");
+
+    if (dark_button.innerHTML == "Dark Theme") {
+        newnote_button.style.backgroundColor = "#2e4a3d";
+        dark_button.innerHTML = "Light Theme";
+        dark_button.style.backgroundColor = "#3d3d3d";
+        save_button.style.backgroundColor = "#2e4a3d";
+        cancel_button.style.backgroundColor = "#822515";
+        aside_area[0].style.backgroundColor = "#707070";
+        content_area[0].style.backgroundColor = "#858282";
+        text_box[0].style.backgroundColor = "black";
+        text_box[0].style.color = "white";
+    }
+    else {
+        newnote_button.style.backgroundColor = "#5F987D";
+        dark_button.innerHTML = "Dark Theme";
+        dark_button.style.backgroundColor = "#696969";
+        save_button.style.backgroundColor = "#5F987D";
+        cancel_button.style.backgroundColor = "#D23C22";
+        aside_area[0].style.backgroundColor = "#C0C0C0";
+        content_area[0].style.backgroundColor = "#ECE8E8";
+        text_box[0].style.backgroundColor = "white";
+        text_box[0].style.color = "black";
+    }
+}
